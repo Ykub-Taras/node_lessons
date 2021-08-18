579c9c5f244b0eb567303289557e30f52d6f34b7
Ykub-Taras <ykub.taras@gmail.com>
2021-08-18 10:24:27 +0300

    lesson_01_HomeWork&Practice

--- index.js
+++ index.js
@@ -102,3 +102,16 @@ fs.readdir('./', {withFileTypes: true}, (error, files) => {
 
 // ********** Practical class 01 **************
 // in progress
+
+const users = [
+    {name: "Olya", gender: "female", age: 21},
+    {name: "Valya", gender: "female", age: 22},
+    {name: "Natasha", gender: "female", age: 23},
+    {name: "Lena", gender: "female", age: 14},
+    {name: "Ulya", gender: "female", age: 15},
+    {name: "Vasya", gender: "male", age: 21},
+    {name: "Petya", gender: "male", age: 22},
+    {name: "Sasha", gender: "male", age: 23},
+    {name: "Seryu", gender: "male", age: 14},
+    {name: "Vitalik", gender: "male", age: 15}
+]
