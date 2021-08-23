1c3cde83e2e4def1c1db6bb2379f679b980559d1
Ykub-Taras <ykub.taras@gmail.com>
2021-08-23 18:45:09 +0300

    lesson 3 HomeWork

--- controllers/withOutRendering/registration.controller.js
+++ controllers/withOutRendering/registration.controller.js
@@ -8,6 +8,7 @@ module.exports = {
 
     postRegistrationPage: (req, res) => {
         const { name, password } = req.body;
+        if (name === undefined || password === undefined) return res.json('Invalid data!');
         const id = users.findIndex((value) => value.name === name);
         if (id > -1) return res.json('This user exist in base!');
         saveNewUser(name, password).then(() => console.log('Saved!'));
