fce8bf010bada50992f3298bc7e1b219588af8f6
Ykub-Taras <ykub.taras@gmail.com>
2021-08-23 20:58:08 +0300

    lesson 3 HomeWork

--- controllers/withOutRendering/authentication.controller.js
+++ controllers/withOutRendering/authentication.controller.js
@@ -6,10 +6,6 @@ module.exports = {
     },
     postUserInfo: (req, res) => {
         const { name, password } = req.body;
-        console.log('post auth');
-        console.log(req.body);
-        console.log(name);
-        console.log(password);
         const id = users.findIndex((value) => value.name === name && value.password === password);
         id > -1 ? res.redirect(`/v2/users/${id}`) : res.redirect('/v2/registration');
     }
