fcab38df03f6edb43ec9a09f31259ee436b125c1
Ykub-Taras <ykub.taras@gmail.com>
2021-09-11 08:54:43 +0300

    lesson 09 HomeWork

---
+++ config/autoGeneratedAdmin.js
@@ -0,0 +1,8 @@
+module.exports = {
+    adminName: 'admin',
+    adminEmail: 'admin@owu.com.ua',
+    adminPassword: 'admin',
+    adminAddress: 'somewhere',
+    adminPhone: '1234567890',
+    adminRole: 'admin',
+};
